0b5bb2a5db57fcfc22a48dae1985d8f7d6df2d11
Rob <roberttcooney@gmail.com>
2017-02-21 20:44:17 +0000

    Semantics

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,7 +1,7 @@
-#Printing Fibonacci sequence up to (n)th number
-a,b,c,n = 0,1,0,10
-while(c<n):
-	print("{i:3} : {n:3}".format(i=c+1,n=a))
-	a,b = b,a+b
-	c+=1
+# Prints fibonacci numbers 1-10
+a, b, c, n = 0, 1, 0, 10
+while c < n:
+    print("{i:3} : {n:3}".format(i=c+1, n=a))
+    a, b = b, a+b
+    c += 1
 
