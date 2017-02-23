8d0239984d0f113b060f81c8bd3b2fdff5c4c356
Rob <roberttcooney@gmail.com>
2017-02-23 12:02:45 +0000

    Minor code optimization

--- DiceRoll.py
+++ DiceRoll.py
@@ -4,5 +4,4 @@ import random
 playing = True
 while playing:
     print("You rolled a {i:0}".format(i=random.randrange(1, 7)))
-    if input("Type Y to play again\n") != 'Y':
-        playing = False
+    playing = input("Type Y to play again\n") == 'Y'
